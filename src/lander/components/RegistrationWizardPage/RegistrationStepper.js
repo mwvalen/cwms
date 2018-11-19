9e9d4f143b5b1762e7a6d8db2f0f35771dd1bad3
mwvalen <mwillvalentine@gmail.com>
2018-11-18 19:18:17 -0500

    Registration stepper no divider on mobile

--- src/lander/components/RegistrationWizardPage/RegistrationStepper.js
+++ src/lander/components/RegistrationWizardPage/RegistrationStepper.js
@@ -3,13 +3,16 @@ import {
   Step,
   Stepper,
   StepLabel,
+  StepConnector
 } from 'material-ui/Stepper'
+import { Divider } from 'material-ui/Divider'
 import withWidth from 'material-ui/utils/withWidth'
 
 export default withWidth()(props => {
   let orientation = props.width > 1 ? "horizontal" : "vertical"
   return (
-    <Stepper activeStep={props.activeStep} orientation={orientation} >
+    <Stepper activeStep={props.activeStep} orientation={orientation}
+        connector={props.width <= 1 ? null : undefined}>      
       <Step>
         <StepLabel>Select your courses</StepLabel>
       </Step>
